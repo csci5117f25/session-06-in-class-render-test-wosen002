1b94deee52d052042876a635c2d220c6b1eefbfe
michael <wosen002@umn.edu>
2025-10-01 15:31:10 -0500

    added survey

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import db
 
 app = Flask(__name__)
+db.setup()
 
-@app.route('/')
-@app.route('/<name>')
+
+@app.route("/")
+@app.route("/<name>")
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template("hello.html", name=name, guestbook=db.get_guestbook())
+
+
+@app.post("/submit")
+def submit():
+    name = request.form.get("name")
+    text = request.form.get("text")
+    db.add_post(name, text)
+    return render_template("hello.html", name=None, guestbook=db.get_guestbook())
